4ff3147259a8ca5e0c45637d430ec291da57059d
SLT-DJH <jydwww@naver.com>
2020-09-20 15:26:39 +0900

    audio test app

---
+++ app/src/main/java/com/example/chapter4audio/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.chapter4audio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void play(View view){
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.start();
+    }
+
+    public void pause(View view){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
+
+
+    }
